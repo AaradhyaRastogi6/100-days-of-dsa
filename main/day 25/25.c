f8eb4c8001b5d45a5acee1ec70342d94fff562bc
AaradhyaRastogi6 <aaradhyarastogi6@gmail.com>
2026-02-26 00:04:23 +0530

    .

---
+++ main/day 25/25.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    struct Node* head = NULL;
+    struct Node* temp = NULL;
+    struct Node* newNode = NULL;
+
+    // Create linked list
+    for (int i = 0; i < n; i++) {
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+        scanf("%d", &newNode->data);
+        newNode->next = NULL;
+
+        if (head == NULL) {
+            head = newNode;
+            temp = newNode;
+        } else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+
+    int key;
+    scanf("%d", &key);
+
+    // Count occurrences
+    int count = 0;
+    temp = head;
+    while (temp != NULL) {
+        if (temp->data == key)
+            count++;
+        temp = temp->next;
+    }
+
+    printf("%d", count);
+
+    return 0;
+}
